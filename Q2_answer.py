66a98eca08191de8a65d80491346912104fbd02d
joonavel@gmail.com <joonavel@gmail.com>
2023-02-02 16:05:54 +0900

    Q2

---
+++ Q2_answer.py
@@ -0,0 +1,21 @@
+# 재귀 함수를 통해 사이클 수 구하기
+    # 재귀 할때마다 cnt += 1
+    # 재귀 중 원래의 값과 같은 값이 나오면 return
+        # 단, 사이클이 1 이상일 경우
+    
+
+def find_cycle(origin, now, cnt):
+    if cnt > 0:
+        if origin == now:
+            return cnt
+
+    temp = now // 10 + now % 10
+    nxt = 10 * (now % 10) + temp % 10
+    
+    return find_cycle(origin, nxt, cnt + 1)
+
+N = int(input())
+
+count = find_cycle(N, N, 0)
+
+print(count)
